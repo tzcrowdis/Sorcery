9566c17ae43afc2740a3e6ff3b578704a934fb0b
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2025-09-04 16:42:09 -0600

    Initial commit

---
+++ Source/Sorcery/Sorcery.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Sorcery : ModuleRules
+{
+	public Sorcery(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
